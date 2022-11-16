2c8ae8379a34eafd0950227608100b6d80fc6379
Sam Daniel <samdanielp@gmail.com>
2022-11-16 07:30:37 +0530

    Scrolling implemented

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
 let imageArray = [];
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 const count = 10;
-const api_key = "w8f8DAWFBS2Z3DNZVYenRjJXpsKO7CPNK1erzJDPZTs";
+const api_key = "API_KEY";
 const api_url = `https://api.unsplash.com/photos/random?client_id=${api_key}&count=${count}`;
 
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+  }
+}
+
 function displayPhoto() {
+  imagesLoaded = 0;
+  totalImages = imageArray.length;
   imageArray.forEach((photo) => {
     const item = document.createElement("a");
     item.setAttribute("href", photo.links.html);
@@ -16,6 +29,7 @@ function displayPhoto() {
     img.setAttribute("src", photo.urls.regular);
     img.setAttribute("alt", photo.alt_description);
     img.setAttribute("title", photo.alt_description);
+    img.addEventListener("load", imageLoaded);
     item.appendChild(img);
     imageContainer.appendChild(item);
   });
@@ -30,6 +44,17 @@ async function getPhotos() {
     console.log(error);
   }
 }
+
+// Infinite Scroll functionality
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+  }
+});
 
 // OnLoad
 getPhotos();
